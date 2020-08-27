2e9a09702e8b45f21ec3a51b298d606021898e89
Matthias C <matts.chory@gmx.at>
2020-08-27 02:31:37 +0200

    added FPSCounter

---
+++ DOTS_PerformanceDemo/Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    private float intervall = 1f;
+    private float nextValueTime;
+
+    private List<int> fpsList;
+    bool addToList = true;
+
+    private void Start()
+    {
+        fpsList = new List<int>();
+        StartCoroutine(ExecuteAfterTime(20));
+    }
+    
+    void Update()
+    {
+        if (Time.unscaledTime > nextValueTime)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            if (addToList)
+                fpsList.Add(fps);
+            nextValueTime = Time.unscaledTime + intervall;
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        addToList = false;
+        fpsList.Remove(0);
+        string allValues = string.Join(",", fpsList.ToArray());
+        Debug.Log("Values (" + fpsList.Count + "): " + allValues);
+        Debug.Log("Highest: " + fpsList.Max());
+        Debug.Log("Lowest: " + fpsList.Min());
+        Debug.Log("Average: " + fpsList.Average());
+    }
+}
